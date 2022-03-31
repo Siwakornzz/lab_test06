9cc0b4d52491b1ca7ea38b696dd6541abdaab2ae
Siwakornzz <Siwakornzz@hotmail.com>
2022-04-01 02:18:39 +0700

    lab10

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routes.year_route import year_api_router
-from routes.todos_route import todo_api_router
-
-app = FastAPI() 
-
-app.include_router(year_api_router)
-app.include_router(todo_api_router)
